bde8bbf03ce0f76d7c78e50fc73740c124f7bab8
haridevdevnarayan <133562982+haridevdevnarayan@users.noreply.github.com>
2023-05-14 17:41:41 +0530

    Initial commit

---
+++ App.tsx
@@ -0,0 +1,201 @@
+import * as React from 'react';
+import './style.css';
+
+export default function App() {
+  return (
+    <div>
+      <h1>Hello StackBlitz!</h1>
+      <p>Start editing to see some magic happen :)</p>
+
+      <div className="landing">
+        <div className="bar-container">
+          <div className="title-bar-3">
+            <div className="title-bar-3-1">
+              <div className="logo-variation">
+                <img
+                  className="frame-6"
+                  src="https://i.ibb.co/RHWyK6g/logo-Sheet.png"
+                  alt="Frame 6"
+                />
+              </div>
+              <div className="frame-427319834">
+                <div className="frame-427319833">
+                  <div className="hello-ashfak valign-text-middle">
+                    Sheet Era
+                  </div>
+                </div>
+                <div className="combined-shape-2">
+                  <img
+                    className="combined-shape"
+                    src="https://i.ibb.co/h7LTwpb/combined-shape.png"
+                    alt="Combined Shape"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="frame-427319870">
+          <p>How Would You Like to Get Started?</p>
+
+          <div className="frame-427319868">
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <img
+                  className="icon"
+                  src="https://i.ibb.co/J2d7PvL/icon.png"
+                  alt="Icon"
+                />
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Data Sync
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <img
+                  className="icon-1"
+                  src="https://i.ibb.co/WByHbw5/icon-1.png"
+                  alt="Icon"
+                />
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Rest Hook
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <img
+                  className="icon"
+                  src="https://i.ibb.co/0fr1jH7/icon-2.png"
+                  alt="Icon"
+                />
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Workflow Automation
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <div className="icon-2">
+                  <div className="frame-427319886">
+                    <div className="group-33619">
+                      <div className="overlap-group1">
+                        <div className="union-container">
+                          <img
+                            className="union"
+                            src="https://i.ibb.co/6s2Cxt6/union.png"
+                            alt="Union"
+                          />
+                          <img
+                            className="union-1"
+                            src="https://i.ibb.co/C8LQ3s9/union-1.png"
+                            alt="Union"
+                          />
+                        </div>
+                        <img
+                          className="vector"
+                          src="https://i.ibb.co/G0MNT75/vector.png"
+                          alt="Vector"
+                        />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Webhook
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <img
+                  className="icon"
+                  src="https://i.ibb.co/QM4mhZ8/icon-3.png"
+                  alt="Icon"
+                />
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Sheet Talk
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <img
+                  className="icon"
+                  src="https://i.ibb.co/b1NHnRY/icon-4.png"
+                  alt="Icon"
+                />
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Party Sheet
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="frame-4273198">
+              <div className="frame-427319811">
+                <img
+                  className="icon"
+                  src="https://i.ibb.co/FJhcGTL/icon-5.png"
+                  alt="Icon"
+                />
+                <div className="frame-427319810">
+                  <div className="page-name-page-name satoshivariable-bold-black-20px">
+                    Sheet Template
+                  </div>
+                  <div className="page-name-page-name-1 dmsans-normal-fuscous-gray-12px">
+                    Lorem Nipsum Some Description...
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="frame-427319869">
+          <img
+            className="frame-427319891"
+            src="https://i.ibb.co/3m1C2NV/frame-427319891.png"
+            alt="Frame 427319891"
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# react-ts-keuxv8
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-ts-keuxv8)
